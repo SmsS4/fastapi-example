6f39129c9bff4312f42039b9b39997aaef725ea7
SmsS4 <smss.lite@gmail.com>
2023-01-17 12:05:57 +0330

    Add bg task

--- example/main.py
+++ example/main.py
@@ -4,10 +4,39 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import PlainTextResponse
 
 import time
-
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
 
 def main() -> None:
-    app = FastAPI()
+    app = FastAPI(
+        title="ChimichangApp",
+        description='description',
+        version="0.0.1",
+        terms_of_service="http://example.com/terms/",
+        contact={
+            "name": "Deadpoolio the Amazing",
+            "url": "http://x-force.example.com/contact/",
+            "email": "dp@x-force.example.com",
+        },
+        license_info={
+            "name": "Apache 2.0",
+            "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+        },
+        openapi_tags=tags_metadata,
+        openapi_url="/api/v1/openapi.json",
+    )
 
     @app.exception_handler(RequestValidationError)
     async def validation_exception_handler(request, exc):
