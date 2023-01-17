f0540f732db90d55bf66b303257e02a95b2772d9
SmsS4 <smss.lite@gmail.com>
2023-01-17 12:01:58 +0330

    Add middleware

--- example/main.py
+++ example/main.py
@@ -1,7 +1,9 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import PlainTextResponse
+
+import time
 
 
 def main() -> None:
@@ -10,6 +12,14 @@ def main() -> None:
     @app.exception_handler(RequestValidationError)
     async def validation_exception_handler(request, exc):
         return PlainTextResponse(str(exc), status_code=400)
+
+    @app.middleware("http")
+    async def add_process_time_header(request: Request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        response.headers["X-Process-Time"] = str(process_time)
+        return response
 
     uvicorn.run(app, host='127.0.0.1', port=8000)
 
